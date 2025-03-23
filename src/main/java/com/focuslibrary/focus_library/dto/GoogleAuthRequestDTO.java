a5d194ce82344ef02be939a39251da55b4482d6a
Douglas Alves <douglas.sousa@wildlifestudios.com>
2025-03-23 13:35:32 -0300

    adicionando registro com google

---
+++ src/main/java/com/focuslibrary/focus_library/dto/GoogleAuthRequestDTO.java
@@ -0,0 +1,16 @@
+package com.focuslibrary.focus_library.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoogleAuthRequestDTO {
+    @NotBlank(message = "Token do Google é obrigatório")
+    private String token;
+} 
---
+++ src/main/java/com/focuslibrary/focus_library/repository/GoogleAccountRepository.java
@@ -0,0 +1,12 @@
+package com.focuslibrary.focus_library.repository;
+
+import com.focuslibrary.focus_library.model.GoogleAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GoogleAccountRepository extends JpaRepository<GoogleAccount, String> {
+    Optional<GoogleAccount> findByGoogleId(String googleId);
+} 
